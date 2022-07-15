9bbfa66edfee31d1de3a88fa847dbe14b8729a56
LavredisG <el18014@mail.ntua.gr>
2022-07-15 03:10:19 +0300

    Changes to main(Charts) page

--- FrontEnd/Charts/Charts.js
+++ FrontEnd/Charts/Charts.js
@@ -1,25 +1,50 @@
-// let namex = sessionStorage.getItem("first_name");
-
+document.addEventListener("DOMContentLoaded", function(){
+    // alert(localStorage.getItem("email"))
+    document.getElementById("email").innerHTML = localStorage.getItem("email");
+    document.getElementById("first_name").innerHTML = localStorage.getItem("first_name");
+    document.getElementById("last_name").innerHTML = localStorage.getItem("last_name");
     
-document.addEventListener("DOMContentLoaded", function () {
-    let email = location.search.substring(1);
-    // let a = first_last.split("|");
-    // window.alert(email_query);
-    // let email = a[0];
-    // let last = a[1];
-    // document.getElementById("first_last_name").innerHTML = "Hello " + email;
-    // let quantity = document.getElementById("quantity")
+})
+
+
+//This function either hides or displays the generation type
+//options based on the quantity option
+function get_quantity(){
+    
     let quantity = document.querySelector("select[name=quantity]").value;
     let gen_type = document.querySelector("li[id=gen_type]");
-    // alert(typeof(gen_type));  
-    alert(quantity);  
-    // if(quantity == "type"){
-    //     alert("yo");
-    // }
-    
-    //alert("Page Loaded...");
+    if(quantity == "type") gen_type.style.visibility = "visible";
+    else gen_type.style.visibility = "hidden";
 
-   
+}
+
+function logout(){
+    window.location.assign("../SSO/SSO.html");
+}
+// let namex = sessionStorage.getItem("first_name");
 
 
-})
+// document.addEventListener("DOMContentLoaded", function () {
+    //     let email = location.search.substring(1);
+    //     // let a = first_last.split("|");
+    //     // window.alert(email_query);
+    //     // let email = a[0];
+    //     // let last = a[1];
+    //     // document.getElementById("first_last_name").innerHTML = "Hello " + email;
+    //     // let quantity = document.getElementById("quantity")
+    // let quantity = document.querySelector("select[name=quantity]").value;
+    // let gen_type = document.querySelector("li[id=gen_type]");
+    //     // alert(typeof(gen_type));  
+    //     alert(quantity);  
+    //     // if(quantity == "type"){
+        //     //     alert("yo");
+        //     // }
+        
+        //     //alert("Page Loaded...");
+        
+        
+        
+        
+
+
+// })
--- FrontEnd/SSO/SSO.js
+++ FrontEnd/SSO/SSO.js
@@ -1,6 +1,30 @@
-function handleCredentialResponse(data){
+let userJWT
+const base_url = "https://oauth2.googleapis.com/tokeninfo?id_token=";
 
-    window.alert("hello from signin");
-    console.log(data);
+function handleCredentialResponse(data) {
+    
+    // console.log(data);
+    fetchUserDetails(data["credential"])
+    window.location.assign("../Charts/Charts.html");
+    
+}
 
+function fetchUserDetails(cred) {
+    const xhr = new XMLHttpRequest();
+    const url = base_url + cred;
+    xhr.open("GET", url);
+    xhr.send(); 
+    
+    xhr.onreadystatechange = (e) => { 
+        if(xhr.readyState === XMLHttpRequest.DONE){
+            console.log(xhr.responseText);
+            const parsedResponse = JSON.parse(xhr.response);
+            // window.alert("User " + parsedResponse["given_name"] + " " + parsedResponse["family_name"] + " logged in!")
+            localStorage.setItem("email", parsedResponse["email"]);
+            localStorage.setItem("first_name", parsedResponse["given_name"]);
+            localStorage.setItem("last_name", parsedResponse["family_name"]);
+
+        }
+    }
+    
 }
